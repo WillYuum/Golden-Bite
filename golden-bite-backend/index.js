950d4206e15b090e1e02083c7cd1d2475e6dc5c8
William <liams_1998@hotmail.com>
2019-06-27 08:26:25 +0300

    added React to project

--- golden-bite-backend/index.js
+++ golden-bite-backend/index.js
@@ -2,15 +2,14 @@ const express = require('express')
 const app = express()
 
 
+
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./goldenbites')
 
 
-app.get("/read/products", function(req,res){
-
+app.get("/products/read", function(req,res){
 
     let sql = "Select * from products";
-
 
     db.all(sql,[], (err,rows)=>{
         data = []
@@ -23,8 +22,37 @@ app.get("/read/products", function(req,res){
 })
 
 
+app.get("/products/delete/:id?" ,function(req,res){
+        ID = req.params.id
+        console.log(ID)
+        
+        db.run(`DELETE FROM products WHERE products_id=${ID}`, function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log(`Product ${ID} got deleted!`)
+            }
+        })
+    })
 
+    app.get("/products/edit/:id?", function(req,res){
 
+        const ID = req.params.id
 
+        const PRODUCTS_ID = req.query.products_name
+        
+        db.run(`UPDATE products SET products_name = ?
 
-app.listen(3000);
+                WHERE products_id= ? `,[PRODUCTS_ID,ID] ,function(err){
+            if(err){
+                console.log(err)
+            }else{
+                console.log(`edited product ${ID} with ${PRODUCTS_ID}`)
+            }
+        })
+
+    })
+
+ 
+
+app.listen(3001);
---
+++ golden-bites/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      data : []
+    }
+  }
+
+  
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
