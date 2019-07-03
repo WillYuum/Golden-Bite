d29aa38e5fb778654166549f5db6dc1dff4d1429
William <liams_1998@hotmail.com>
2019-07-03 11:00:45 +0300

    turned App.js to a class Component

--- goldenbites-app/src/App.js
+++ goldenbites-app/src/App.js
@@ -3,15 +3,21 @@ import React from 'react';
 import './App.css';
 import BlogPage from './Components/Blogs/BlogPage';
 
-function App() {
-  return (
-    <div className="App">
-     <BlogPage/>
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
 
+    }
+  }
 
-
-    </div>
-  );
+  render(){
+    return(
+      <div className = "App-container">
+        <BlogPage/>
+      </div>
+    );
+  }
 }
 
 export default App;
