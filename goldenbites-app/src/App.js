cc78f118356784e74081ea8f10edfdaa8798fcde
William <liams_1998@hotmail.com>
2019-07-04 16:12:45 +0300

    fixing App.js

--- goldenbites-app/src/App.js
+++ goldenbites-app/src/App.js
@@ -4,30 +4,10 @@ import "./App.css";
 import BlogPage from "./Components/Blogs/BlogPage";
 import MainBlogClick from "./Components/Blogs/MainBlogClick";
 
-<<<<<<< HEAD
 
 import ProductsPage from "./Components/Products/ProductPage";
 
 class App extends Component {
-=======
-class App extends Component {
-
-<<<<<<< HEAD
-  render() {
- 
-
-  
-    return (
-      <div className="App">
-=======
-
-//----Products Components------
- import ProductPage from "./Components/Products/ProductPage"
-//--------- --------------------
-class App extends React.Component{
->>>>>>> 4648882d2c5e2fb70166691c214708731ba92552
->>>>>>> 19949f511f35779d1aae2367d2fbc8594d244dd5
-
   render() {
     return (
       <div className="App">
@@ -35,30 +15,10 @@ class App extends React.Component{
           <Route path="/MainBlogClick" component={MainBlogClick} />
           <Route path="/" exact component={BlogPage} />
 
-<<<<<<< HEAD
+
           <Route path="/Products" component={ProductsPage}/>
         </Router>
-      
-=======
-<<<<<<< HEAD
 
-
-
-        <Router>
-          <Route path="/MainBlogClick" component={MainBlogClick} />
-          <Route path="/" exact component={BlogPage} />
-        </Router>
-=======
-  render(){
-    return(
-      <div className = "App-container">
-        {/* <BlogPage /> */}
-
-        <ProductPage />
-        
->>>>>>> 19949f511f35779d1aae2367d2fbc8594d244dd5
-        
->>>>>>> 4648882d2c5e2fb70166691c214708731ba92552
       </div>
     );
   }
