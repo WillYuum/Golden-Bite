ac4d973c56e93c65dd4d60bfa8a92e25a7bf2d4d
William <liams_1998@hotmail.com>
2019-07-08 09:47:16 +0300

    fixed merge error in App.js

--- goldenbites-app/src/App.js
+++ goldenbites-app/src/App.js
@@ -3,20 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import BlogPage from "./Components/Blogs/BlogPage";
 import MainBlogClick from "./Components/Blogs/MainBlogClick";
-
-
-<<<<<<< HEAD
-  render() {
  
-=======
-import ProductsPage from "./Components/Products/ProductPage";
->>>>>>> 14c57c21fb9a5e8ba3e8c7f92ab5a5ad724c88ec
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
 
    
   
@@ -25,17 +17,11 @@ class App extends Component {
 
 
 
-=======
->>>>>>> 14c57c21fb9a5e8ba3e8c7f92ab5a5ad724c88ec
         <Router>
           <Route path="/MainBlogClick" component={MainBlogClick} />
           <Route path="/" exact component={BlogPage} />
           <Route path="/Products" component={ProductsPage}/>
         </Router>
-<<<<<<< HEAD
-=======
-
->>>>>>> 14c57c21fb9a5e8ba3e8c7f92ab5a5ad724c88ec
       </div>
     );
   }
