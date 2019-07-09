6d0606d4dbb7033fed9e3512430c17b7f7d7a09d
William <liams_1998@hotmail.com>
2019-07-09 12:58:28 +0300

    added link to mainblog

--- goldenbites-app/src/Components/Blogs/MainBlogClick.js
+++ goldenbites-app/src/Components/Blogs/MainBlogClick.js
@@ -1,108 +1,144 @@
-import React,{Component} from 'react'
-import BlogPage from './BlogPage';
-
-
+import React, { Component } from "react";
+import BlogPage from "./BlogPage";
 
 class MainBlogClick extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: [],
-      
-    }
-	}
-	
+      data: []
+    };
+  }
+
   async componentDidMount() {
-    try{
+    try {
       // FETCH FROM IMAGES_BLOGS TABLE
-      const responsa= await fetch("http://localhost:3001/images_blogs/read");
-      const iiiblogs = await responsa.json()
-      await this.setState({data:iiiblogs.DATA})
-      await console.log("DATA:",this.state.data)
- // FETCH FROM BLOGS TABLE author
- const responseau  = await fetch("http://localhost:3001/blogs/read");
- const blogsau = await responseau.json()
- await this.setState({data:blogsau.DATA})
- await console.log("DATA:",this.state.data)
+      const responsa = await fetch("http://localhost:3001/images_blogs/read");
+      const iiiblogs = await responsa.json();
+      await this.setState({ data: iiiblogs.DATA });
+      await console.log("DATA:", this.state.data);
+      // FETCH FROM BLOGS TABLE author
+      const responseau = await fetch("http://localhost:3001/blogs/read");
+      const blogsau = await responseau.json();
+      await this.setState({ data: blogsau.DATA });
+      await console.log("DATA:", this.state.data);
 
-  // FETCH FROM BLOGS TABLE date
-  const responsedt  = await fetch("http://localhost:3001/blogs/read");
-  const datedt = await responsedt.json()
-  await this.setState({data:datedt.DATA})
-  await console.log("DATA:",this.state.data)
+      // FETCH FROM BLOGS TABLE date
+      const responsedt = await fetch("http://localhost:3001/blogs/read");
+      const datedt = await responsedt.json();
+      await this.setState({ data: datedt.DATA });
+      await console.log("DATA:", this.state.data);
 
-   // FETCH FROM BLOGS TABLE content
-   const responsect = await fetch("http://localhost:3001/blogs/read");
-   const contentct= await responsect.json()
-   await this.setState({data:contentct.DATA})
-   await console.log("DATA:",this.state.data)
-    }catch(err){
-      console.log(err)
+      // FETCH FROM BLOGS TABLE content
+      const responsect = await fetch("http://localhost:3001/blogs/read");
+      const contentct = await responsect.json();
+      await this.setState({ data: contentct.DATA });
+      await console.log("DATA:", this.state.data);
+    } catch (err) {
+      console.log(err);
     }
   }
-render()
-{
+  render() {
+    return (
+      <div className="mainblogclick">
+        <div className="mainimage">
+          <img
+            width="300px"
+            height="250px"
+            src={"http://localhost:3000/static/media/blogItemLg.8f802f73.jpg"}
+            alt=""
+          />
+          {/* <img  src="./cake.jpg"/> */}
+          <div className="overlayy">
+            <div className="text">Yummy!</div>
+          </div>
+        </div>
+        <hr className="style-seven" />
+        <div className="headundermainimage">
+          <p>
+            By Chef{" "}
+            {this.state.data.length > 0
+              ? this.state.data[0].blogs_author
+              : null}
+          </p>
+          <p>
+            {this.state.data.length > 0 ? this.state.data[0].blogs_date : null}
+          </p>
+        </div>
+        <div />
+        <div className="maintextblog">
+          <h1>How it all started!</h1>
 
-return(
-
-<div className="mainblogclick">
-
-<div className="mainimage">
-<img
-                  width="300px"
-                  height="250px"
-                  src={"http://localhost:3000/static/media/blogItemLg.8f802f73.jpg"}
-                  alt=""
-                />
-{/* <img  src="./cake.jpg"/> */}
-<div className="overlayy">
-    <div className="text">Yummy!</div>
-  </div>
-</div>
-<hr className="style-seven"/>
-<div className="headundermainimage">
-
-<p>By Chef {this.state.data.length>0? this.state.data[0].blogs_author: null}</p>
-<p>{this.state.data.length>0? this.state.data[0].blogs_date: null}</p>
-</div>
-<div>
-
-</div>
-<div className="maintextblog">
-<h1>How it all started!</h1>
-
- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium lectus quam id leo. Orci phasellus egestas tellus rutrum tellus pellentesque. Mi eget mauris pharetra et ultrices neque ornare aenean. Massa enim nec dui nunc mattis enim. Lacus luctus accumsan tortor posuere ac ut consequat semper viverra. Suspendisse potenti nullam ac tortor vitae. Scelerisque mauris pellentesque pulvinar pellentesque. Pharetra et ultrices neque ornare aenean euismod elementum. A iaculis at erat pellentesque adipiscing. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Dis parturient montes nascetur ridiculus. Sit amet facilisis magna etiam tempor orci eu.
-
-Neque vitae tempus quam pellentesque nec. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Hendrerit gravida rutrum quisque non tellus orci ac. Eu feugiat pretium nibh ipsum consequat nisl. Sem fringilla ut morbi tincidunt augue interdum velit. Sit amet tellus cras adipiscing enim eu turpis egestas. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Dolor morbi non arcu risus quis. Non consectetur a erat nam at lectus urna. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Feugiat scelerisque varius morbi enim nunc faucibus a. Sem integer vitae justo eget magna fermentum iaculis eu non. Risus commodo viverra maecenas accumsan lacus vel facilisis. Ullamcorper morbi tincidunt ornare massa. Pellentesque habitant morbi tristique senectus et netus. Aliquam ultrices sagittis orci a scelerisque purus. Consequat ac felis donec et odio pellentesque diam. Non blandit massa enim nec dui nunc. Arcu non sodales neque sodales. Sed cras ornare arcu dui vivamus.
-
-Nam libero justo laoreet sit. Lectus nulla at volutpat diam ut venenatis tellus in. Habitant morbi tristique senectus et netus et malesuada. Morbi leo urna molestie at elementum eu facilisis sed odio. Sed faucibus turpis in eu mi. Non odio euismod lacinia at quis risus sed vulputate. Tempus urna et pharetra pharetra massa massa ultricies. A iaculis at erat pellentesque adipiscing commodo elit at. Venenatis tellus in metus vulputate eu. Curabitur gravida arcu ac tortor dignissim convallis aenean. Nulla malesuada pellentesque elit eget gravida cum sociis.
-
-Vulputate sapien nec sagittis aliquam malesuada bibendum. Ultricies lacus sed turpis tincidunt id. Pretium fusce id velit ut tortor pretium viverra suspendisse potenti. Urna porttitor rhoncus dolor purus non enim praesent. Cursus turpis massa tincidunt dui ut ornare lectus sit. Sagittis orci a scelerisque purus semper. Tristique senectus et netus et malesuada fames. Quam nulla porttitor massa id neque aliquam. Tortor posuere ac ut consequat semper viverra nam libero justo. Morbi quis commodo odio aenean sed adipiscing diam donec adipiscing. Nibh mauris cursus mattis molestie a iaculis at. Arcu vitae elementum curabitur vitae nunc sed velit dignissim. Morbi enim nunc faucibus a pellentesque. Amet consectetur adipiscing elit duis tristique sollicitudin nibh sit. Maecenas ultricies mi eget mauris pharetra et ultrices.
-
-Elementum curabitur vitae nunc sed. Nunc sed id semper risus. Cras adipiscing enim eu turpis. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non. Leo in vitae turpis massa sed. Amet est placerat in egestas erat imperdiet. Urna cursus eget nunc scelerisque viverra mauris in. Aliquet bibendum enim facilisis gravida. Felis bibendum ut tristique et egestas quis. Scelerisque fermentum dui faucibus in. Quis varius quam quisque id diam. Eget nunc scelerisque viverra mauris in aliquam sem. Nibh venenatis cras sed felis eget velit aliquet.
-
-Duis at consectetur lorem donec massa sapien faucibus. Quis hendrerit dolor magna eget. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Velit euismod in pellentesque massa placerat duis. Turpis massa tincidunt dui ut ornare lectus sit amet. Imperdiet nulla malesuada pellentesque elit eget gravida cum. Tincidunt lobortis feugiat vivamus at. Commodo quis imperdiet massa tincidunt nunc pulvinar. Orci sagittis eu volutpat odio facilisis mauris sit amet massa. Nulla pellentesque dignissim enim sit.</p>
-
-</div>
-
-
-</div>
-
-
-)
-
-
-
-
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium
+            lectus quam id leo. Orci phasellus egestas tellus rutrum tellus
+            pellentesque. Mi eget mauris pharetra et ultrices neque ornare
+            aenean. Massa enim nec dui nunc mattis enim. Lacus luctus accumsan
+            tortor posuere ac ut consequat semper viverra. Suspendisse potenti
+            nullam ac tortor vitae. Scelerisque mauris pellentesque pulvinar
+            pellentesque. Pharetra et ultrices neque ornare aenean euismod
+            elementum. A iaculis at erat pellentesque adipiscing. Pharetra
+            pharetra massa massa ultricies mi quis hendrerit dolor magna. Dis
+            parturient montes nascetur ridiculus. Sit amet facilisis magna etiam
+            tempor orci eu. Neque vitae tempus quam pellentesque nec. Ornare
+            aenean euismod elementum nisi quis eleifend quam adipiscing vitae.
+            Hendrerit gravida rutrum quisque non tellus orci ac. Eu feugiat
+            pretium nibh ipsum consequat nisl. Sem fringilla ut morbi tincidunt
+            augue interdum velit. Sit amet tellus cras adipiscing enim eu turpis
+            egestas. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Dolor
+            morbi non arcu risus quis. Non consectetur a erat nam at lectus
+            urna. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet.
+            Feugiat scelerisque varius morbi enim nunc faucibus a. Sem integer
+            vitae justo eget magna fermentum iaculis eu non. Risus commodo
+            viverra maecenas accumsan lacus vel facilisis. Ullamcorper morbi
+            tincidunt ornare massa. Pellentesque habitant morbi tristique
+            senectus et netus. Aliquam ultrices sagittis orci a scelerisque
+            purus. Consequat ac felis donec et odio pellentesque diam. Non
+            blandit massa enim nec dui nunc. Arcu non sodales neque sodales. Sed
+            cras ornare arcu dui vivamus. Nam libero justo laoreet sit. Lectus
+            nulla at volutpat diam ut venenatis tellus in. Habitant morbi
+            tristique senectus et netus et malesuada. Morbi leo urna molestie at
+            elementum eu facilisis sed odio. Sed faucibus turpis in eu mi. Non
+            odio euismod lacinia at quis risus sed vulputate. Tempus urna et
+            pharetra pharetra massa massa ultricies. A iaculis at erat
+            pellentesque adipiscing commodo elit at. Venenatis tellus in metus
+            vulputate eu. Curabitur gravida arcu ac tortor dignissim convallis
+            aenean. Nulla malesuada pellentesque elit eget gravida cum sociis.
+            Vulputate sapien nec sagittis aliquam malesuada bibendum. Ultricies
+            lacus sed turpis tincidunt id. Pretium fusce id velit ut tortor
+            pretium viverra suspendisse potenti. Urna porttitor rhoncus dolor
+            purus non enim praesent. Cursus turpis massa tincidunt dui ut ornare
+            lectus sit. Sagittis orci a scelerisque purus semper. Tristique
+            senectus et netus et malesuada fames. Quam nulla porttitor massa id
+            neque aliquam. Tortor posuere ac ut consequat semper viverra nam
+            libero justo. Morbi quis commodo odio aenean sed adipiscing diam
+            donec adipiscing. Nibh mauris cursus mattis molestie a iaculis at.
+            Arcu vitae elementum curabitur vitae nunc sed velit dignissim. Morbi
+            enim nunc faucibus a pellentesque. Amet consectetur adipiscing elit
+            duis tristique sollicitudin nibh sit. Maecenas ultricies mi eget
+            mauris pharetra et ultrices. Elementum curabitur vitae nunc sed.
+            Nunc sed id semper risus. Cras adipiscing enim eu turpis. Venenatis
+            lectus magna fringilla urna porttitor rhoncus dolor purus non. Leo
+            in vitae turpis massa sed. Amet est placerat in egestas erat
+            imperdiet. Urna cursus eget nunc scelerisque viverra mauris in.
+            Aliquet bibendum enim facilisis gravida. Felis bibendum ut tristique
+            et egestas quis. Scelerisque fermentum dui faucibus in. Quis varius
+            quam quisque id diam. Eget nunc scelerisque viverra mauris in
+            aliquam sem. Nibh venenatis cras sed felis eget velit aliquet. Duis
+            at consectetur lorem donec massa sapien faucibus. Quis hendrerit
+            dolor magna eget. Aliquam purus sit amet luctus venenatis lectus
+            magna fringilla urna. Velit euismod in pellentesque massa placerat
+            duis. Turpis massa tincidunt dui ut ornare lectus sit amet.
+            Imperdiet nulla malesuada pellentesque elit eget gravida cum.
+            Tincidunt lobortis feugiat vivamus at. Commodo quis imperdiet massa
+            tincidunt nunc pulvinar. Orci sagittis eu volutpat odio facilisis
+            mauris sit amet massa. Nulla pellentesque dignissim enim sit.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
-
-
-
-}
-
-
-export default MainBlogClick
+export default MainBlogClick;
