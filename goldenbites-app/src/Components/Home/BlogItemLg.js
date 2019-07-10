863511169d5e90cea8662e2d91ca82d4bdf8ef45
Raed <raed_nb@hotmail.com>
2019-07-10 14:35:13 +0300

    blog1 from database fixed

--- goldenbites-app/src/Components/Home/BlogItemLg.js
+++ goldenbites-app/src/Components/Home/BlogItemLg.js
@@ -3,7 +3,33 @@ import {Link} from "react-router-dom";
 import './blogItemLg.css';
 
 class BlogItemLg extends Component {
-  state = {};
+  // state = {};
+
+
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			data: [],
+			datablogs: [],
+		};
+	}
+
+	async componentDidMount() {
+		try {
+	
+			// FETCH FROM BLOGS TABLE
+			const response_blogs_read = await fetch('http://localhost:3001/blogs/read');
+			const blogs_reading = await response_blogs_read.json();
+			await this.setState({ datablogs: blogs_reading.DATA[0] });
+			await console.log('my blogsnew are:', this.state.datablogs[0].blogs_author);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+
   render() {
     return (
       <div className='blog__container'>
@@ -11,14 +37,11 @@ class BlogItemLg extends Component {
           <div className='blog__picture' />
         </div>
         <div className='blog__container--right'>
-          <div className='blog__title'>Design Your Box</div>
-          <div className='blog__date'>08 July 2019</div>
-          <div className='blog__author'>by Lama</div>
+          <div className='blog__title'>{this.state.datablogs.blogs_title}</div>
+          <div className='blog__date'>{this.state.datablogs.blogs_date}</div>
+          <div className='blog__author'>{this.state.datablogs.blogs_author}</div>
           <div className='blog__summary'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+          {this.state.datablogs.blogs_content}
           </div>
           <div className='blog__button'>
             <Link to ="/MainBlogClick">Read Article</Link>
