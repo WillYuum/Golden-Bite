feb05edc8a0f65e943be25bb03e3cfc800ed8cad
William <liams_1998@hotmail.com>
2019-07-10 14:08:12 +0300

    fixed a merge

--- goldenbites-app/src/Components/Products/ProductsItem.js
+++ goldenbites-app/src/Components/Products/ProductsItem.js
@@ -35,29 +35,6 @@ export default class ProductsItem extends React.Component {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-<<<<<<< HEAD
-
-   submitHandler = async(e)=>{
-     e.preventDefault()
-    let {name,address,phone,email}=this.state
-    console.log({name,address,phone,email})
-
-    try{
-    const UserDetail = await fetch("http://localhost:3001/orders/create",{
-      method:"POST",
-      body:JSON.stringify(this.state),
-      headers:{"Content-Type":"application/json"}
-      
-    })
-      }catch(err){
-        console.log(err)
-      }
-      this.createOrder()
-   }  
-
-   
-
-=======
   submitHandler = () => {
     console.log(this.state);
     const order = fetch('http://localhost:3001/orders/create', {
@@ -72,7 +49,6 @@ export default class ProductsItem extends React.Component {
       .then(response => console.log('Success:', JSON.stringify(response)))
       .catch(error => console.error('Error:', error));
   };
->>>>>>> 33bcad0b0788e4e66840f22c342a13391376554a
 
   render() {
     const { name, email, phone_number, address } = this.state;
