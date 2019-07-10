33bcad0b0788e4e66840f22c342a13391376554a
BringSteam <youssef.charbel@gmail.com>
2019-07-10 12:45:48 +0300

    DB Update v1.0.8

--- goldenbites-app/src/Components/Products/CategoryListing.js
+++ goldenbites-app/src/Components/Products/CategoryListing.js
@@ -1,42 +1,36 @@
-import React from "react";
+import React from 'react';
 
-import "./Style/Category.css";
+import './Style/Category.css';
 
-import CategoryItem from "./CategoryItem";
+import CategoryItem from './CategoryItem';
 
 export default class CategoryListing extends React.Component {
-    state ={
-        data_Categories: []
-      }
-    
-    
-    
-    async componentDidMount(){
-        try{
-          const response  = await fetch("http://localhost:3001/category/read");
-          const categories = await response.json()
-          await this.setState({data_Categories:categories.DATA})
-          await console.log("DATA =>" ,this.state.data_Categories)
-        }catch(err){
-          console.log(err)
-        }
-      }
+  state = {
+    data_Categories: []
+  };
 
-    
+  async componentDidMount() {
+    try {
+      const response = await fetch('http://localhost:3001/category/read');
+      const categories = await response.json();
+      await this.setState({ data_Categories: categories.DATA });
+      await console.log('DATA =>', this.state.data_Categories);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   render() {
     return (
-      <div className="CategoryListing-container">
-        <div className = "CategoryListing-lists">
-          <CategoryItem a = "All" all_button={this.props.all}/>
-        {
-            this.state.data_Categories.map(category =>{
-                return <CategoryItem a = {category.categories_name} b={this.props.a}/>
-            })
-        }
+      <div className='CategoryListing-container'>
+        <div className='CategoryListing-lists'>
+          <CategoryItem a='All' all_button={this.props.all} />
+          {this.state.data_Categories.map(category => {
+            return (
+              <CategoryItem a={category.categories_name} b={this.props.a} />
+            );
+          })}
         </div>
-
-        
       </div>
     );
   }
