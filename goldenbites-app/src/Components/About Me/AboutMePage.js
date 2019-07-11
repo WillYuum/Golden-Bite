b15eec0078061d166af640ab6cc2fcd68083cbf5
BringSteam <youssef.charbel@gmail.com>
2019-07-11 17:51:44 +0300

    About Me Start v1.0.13

---
+++ goldenbites-app/src/Components/About Me/AboutMePage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class AboutMePage extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default AboutMePage;
